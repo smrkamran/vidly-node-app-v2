55f2eb5ec248edfccaa19b48d3b4047e5208c664
Sameer <sameerkamrandev@gmail.com>
2019-09-24 23:08:48 +0500

    Second Commit

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 1024
   },
-  isAdmin: Boolean
+  isAdmin: { type: Boolean, default: true }
 });
 
 userSchema.methods.generateAuthToken = function() {
